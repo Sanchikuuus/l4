d8de15de84b18f616745ff00e479128e7a0f85fa
Sanchikuuus <Sanchikuuus@ukr.net>
2019-06-02 12:27:49 +0300

    added task 2.10

--- l4/ViewController.swift
+++ l4/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(antimat("fuck you bitch"))
+        
     }
-
-
+    
+    
+    func antimat (_ str : String ) -> String {
+        let badWords : Set<String> = ["fuck", "bitch"]
+        var clearString = str
+        
+        for i in badWords {
+            var replaceString = ""
+            for _ in 0..<i.count {
+                replaceString += "*"
+            }
+            clearString = clearString.lowercased().replacingOccurrences(of: i, with: replaceString)
+        }
+        return clearString
+    }
 }
 
