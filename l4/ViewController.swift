59ba0882e8de44e9781be7c668502bc64434b725
Sanchikuuus <Sanchikuuus@ukr.net>
2019-06-02 13:11:00 +0300

    added task 2.8

--- l4/ViewController.swift
+++ l4/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //print(antimat("fuck you bitch"))
-        print(sampler(["lada", "seda", "dela", "tama", "data"], "da"))
-        
+        //print(sampler(["lada", "seda", "dela", "tama", "data"], "da"))
+        print(transliter("ТеСт ТеКсТа ЗаБоРчИкОм"))
     }
     
     
@@ -45,6 +45,19 @@ class ViewController: UIViewController {
         return result
     }
     
-    
+    func transliter(_ str : String) -> String {
+        var convertedStr : String = ""
+        let dictionary: [String: String] = ["а" : "a", "б" : "b", "в" : "v", "г" : "g", "д" : "d", "е" : "e", "ё" : "e", "ж" : "j", "з" : "z", "и" : "i", "й" : "i", "к" : "k", "л" : "l", "м" : "m", "н" : "n", "о" : "o", "п" : "p", "р" : "r", "с" : "s", "т" : "t", "у" : "u", "ф" : "f", "х" : "h", "ц" : "c", "ч" : "ch", "ш" : "sh", "щ" : "sc", "ъ" : "", "ы" : "y", "ь" : "", "э" : "e", "ю" : "iu", "я" : "ia", " ": " "]
+        
+        for i in str {
+            if i.isLowercase {
+                convertedStr += dictionary[String(i)]!
+            } else {
+                convertedStr += dictionary[String(i).lowercased()]!.uppercased()
+            }
+        }
+        
+        return convertedStr
+    }
 }
 
